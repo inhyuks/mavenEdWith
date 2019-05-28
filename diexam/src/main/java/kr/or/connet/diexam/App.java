9be8ef61cbeb4797e9f3d5a9c8416b841c30e119
inhyuks <sih2341@naver.com>
2019-05-28 16:57:33 +0900

    DI 실습을 위한 프로젝트 생성

---
+++ diexam/src/main/java/kr/or/connet/diexam/App.java
@@ -0,0 +1,13 @@
+package kr.or.connet.diexam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
