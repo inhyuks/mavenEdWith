8a51fe772d8f4ae58ff70f4aeeb1323745ffd3be
inhyuks <sih2341@naver.com>
2019-06-04 11:04:19 +0900

    레이어드아키텍쳐 실습 ) 방명록 프로젝트 생성

---
+++ springGuestBook/src/main/java/kr/or/connect/springGuestBook/config/WebMvcContextConfiguration.java
@@ -0,0 +1,44 @@
+package kr.or.connect.springGuestBook.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+@Configuration // 나 설정파일이에요
+@EnableWebMvc // 기본설정은 여기에해주세요
+@ComponentScan(basePackages = {"kr.or.connect.springGuestBook.controller"})
+//controller 읽어와서 사용해요
+public class WebMvcContextConfiguration extends WebMvcConfigurerAdapter {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/META-INF/resources/webjars/").setCachePeriod(31556926);
+        registry.addResourceHandler("/css/**").addResourceLocations("/css/").setCachePeriod(31556926);
+        registry.addResourceHandler("/img/**").addResourceLocations("/img/").setCachePeriod(31556926);
+        registry.addResourceHandler("/js/**").addResourceLocations("/js/").setCachePeriod(31556926);
+    } //독특한요청이들어오는건 여기서 찾아요~ 라는 설정
+ 
+    // default servlet handler를 사용하게 합니다.
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }//매핑정보가없는 url을 사용하게함
+   
+    @Override
+    public void addViewControllers(final ViewControllerRegistry registry) {
+            System.out.println("addViewControllers가 호출됩니다. ");
+        registry.addViewController("/").setViewName("main");
+    } // 특정 url처리를 컨트롤러클래스를 작성하지않고 매핑할수있게함
+    
+    @Bean
+    public InternalResourceViewResolver getInternalResourceViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    } // 뷰
+}
