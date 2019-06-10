0000d82698b28733a26da055d9472bfc9c99f9e3
inhyuks <sih2341@naver.com>
2019-06-10 18:27:53 +0900

    예약_메인페이지 프론트엔드파트 삽입

---
+++ reservation/src/main/java/kr/or/connect/reservation/config/ApplicationConfig.java
@@ -0,0 +1,14 @@
+package kr.or.connect.reservation.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+//설정파일이다
+@Configuration
+//컴포넌트를 읽기위한 지정
+@ComponentScan(basePackages = { "kr.or.connect.reservation.dao", " kr.or.connect.reservation.service"})
+//DBConfig값 사용
+@Import({DBConfig.class})
+public class ApplicationConfig {
+
+}
---
+++ reservation/src/main/java/kr/or/connect/reservation/config/DBConfig.java
@@ -0,0 +1,44 @@
+package kr.or.connect.reservation.config;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.TransactionManagementConfigurer;
+
+@Configuration
+//트랜잭션관련설정을 자동으로 해줌
+@EnableTransactionManagement
+public class DBConfig implements TransactionManagementConfigurer {
+	private String driverClassName = "com.mysql.jdbc.Driver";
+
+	private String url = "jdbc:mysql://localhost:3306/edwith?useUnicode=true&characterEncoding=utf8&useSSL=false";
+
+	private String username = "root";
+
+	private String password = "1234";
+
+	@Bean
+	public DataSource dataSource() {
+		BasicDataSource dataSource = new BasicDataSource();
+		dataSource.setDriverClassName(driverClassName);
+		dataSource.setUrl(url);
+		dataSource.setUsername(username);
+		dataSource.setPassword(password);
+		return dataSource;
+	}
+
+	@Override
+	public PlatformTransactionManager annotationDrivenTransactionManager() {
+		return transactionManger();
+	}
+
+	@Bean
+	public PlatformTransactionManager transactionManger() {
+		return new DataSourceTransactionManager(dataSource());
+	}
+}
